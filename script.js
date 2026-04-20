0330a447d8c824a71d2b7bb1d0bf2b48a80e993b
Maksym Shevchyk <maksim.shevchik@gmail.com>
2026-04-20 15:13:03 +0200

    Formularz (66815)

--- script.js
+++ script.js
@@ -22,3 +22,71 @@ toggleBtn.addEventListener("click", () => {
     projectsSection.style.display = "none";
   }
 });
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let isValid = true;
+
+  const firstName = document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const email = document.getElementById("email");
+  const message = document.getElementById("message");
+
+  const firstNameError = document.getElementById("firstNameError");
+  const lastNameError = document.getElementById("lastNameError");
+  const emailError = document.getElementById("emailError");
+  const messageError = document.getElementById("messageError");
+
+  firstNameError.textContent = "";
+  lastNameError.textContent = "";
+  emailError.textContent = "";
+  messageError.textContent = "";
+
+  
+  if (!firstName.value.trim()) {
+    firstNameError.textContent = "Imię jest wymagane";
+    isValid = false;
+  }
+
+  if (!lastName.value.trim()) {
+    lastNameError.textContent = "Nazwisko jest wymagane";
+    isValid = false;
+  }
+
+  if (!email.value.trim()) {
+    emailError.textContent = "Email jest wymagany";
+    isValid = false;
+  }
+
+  if (!message.value.trim()) {
+    messageError.textContent = "Wiadomość jest wymagana";
+    isValid = false;
+  }
+
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (email.value && !emailPattern.test(email.value)) {
+    emailError.textContent = "Niepoprawny email";
+    isValid = false;
+  }
+
+  const namePattern = /^[A-Za-zÀ-ž\s]+$/;
+
+  if (firstName.value && !namePattern.test(firstName.value)) {
+    firstNameError.textContent = "Imię nie może zawierać cyfr";
+    isValid = false;
+  }
+
+  if (lastName.value && !namePattern.test(lastName.value)) {
+    lastNameError.textContent = "Nazwisko nie może zawierać cyfr";
+    isValid = false;
+  }
+
+  if (isValid) {
+    alert("Formularz wysłany poprawnie");
+    form.reset();
+  }
+});
